282e39961a9abfff9e1a8dbf5a17917de994d747
kaigagi <kaigagipro@gmail.com>
2022-04-19 19:18:52 +0700

    add article

--- src/Parts/News.tsx
+++ src/Parts/News.tsx
@@ -5,17 +5,34 @@ import {Card, CardBody} from "reactstrap"
 //import css
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './News.css';
+import { useRef } from 'react';
+
 
 function News() {
+
+    const articleHoverEffect = useRef<HTMLDivElement>(null)
+
+    // a function to handle css when hover an article
+    function hoverArticleIn() {
+        const element = articleHoverEffect.current !;
+        element.style.display = "block";
+    }
+
+    function hoverArticleOut() {
+        const element = articleHoverEffect.current !;
+        element.style.display = "none";
+    }
+
     return(
         <div className="News d-flex flex-column align-items-center">
             <Title width= {500} height = {250} />
             <br />
             <div className='title'>-----WHAT'S NEW-----</div>
             <br />
-            <Card >
+            <Card className='article-card' onMouseEnter={hoverArticleIn} onMouseLeave={hoverArticleOut}>
+                <div className='article-hover-effect' ref={articleHoverEffect}></div>
                 <CardBody>
-
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi aperiam, optio doloremque vel laudantium natus itaque autem eos tempora libero sunt similique nulla hic amet eius iure ratione ex ab!
                 </CardBody>
             </Card>
         </div>
